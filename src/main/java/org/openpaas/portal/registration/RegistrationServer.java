0d2cbb933e504b6335de307dcde5ab50e29308df
swmoon <swmoon@bluedigm.com>
2018-01-26 10:59:36 +0900

    PAAS-TA-PORTAL-V2-SP1
    PAAS-TA-PORTAL-CONFIG-SERVER Repositories 추가
    PAAS-TA-PORTAL-STORAGE Repositories 추가
    PAAS-TA-PORTAL-COMMON-API Repositories 추가
    
    SpringCloud Edgware.RELEASE 업그레이드
    TomcatEmded 8.5.14 업그레이드
    Gradle 4.4.1 업그레이드
    Spring-boot 1.5.9 업그레이드
    Redis 1.3.1 업데그레이드
    
    Build.Gradle 형식 통일
    
    Config 서비스 추가
    Config Config 서버에서 가져오도록 수정됨
    
    서비스 시작 순서 PAAS-TA-CONFIG-SERVER -> PAAS-TA-REGISTRATION -> 그외의 서비스

--- src/main/java/org/openpaas/portal/registration/RegistrationServer.java
+++ src/main/java/org/openpaas/portal/registration/RegistrationServer.java
@@ -12,17 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class RegistrationServer {
-
-	/**
-	 * Run the application using Spring Boot and an embedded servlet engine.
-	 * 
-	 * @param args
-	 *            Program arguments - ignored.
-	 */
 	public static void main(String[] args) {
-		// Tell server to look for registration.properties or registration.yml
-		System.setProperty("spring.config.name", "registration-server");
-
 		SpringApplication.run(RegistrationServer.class, args);
 	}
 }
