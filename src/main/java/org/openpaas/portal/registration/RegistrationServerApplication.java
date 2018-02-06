88e991ebf37dde301b8acea96d7606ed4c06be1d
swmoon <swmoon@bluedigm.com>
2018-02-06 13:57:13 +0900

    PAAS-TA V2 - build

--- src/main/java/org/openpaas/portal/registration/RegistrationServerApplication.java
+++ src/main/java/org/openpaas/portal/registration/RegistrationServerApplication.java
@@ -12,8 +12,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistrationServer {
+public class RegistrationServerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RegistrationServer.class, args);
+		SpringApplication.run(RegistrationServerApplication.class, args);
 	}
 }
