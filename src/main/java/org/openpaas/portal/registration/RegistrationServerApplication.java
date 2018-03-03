5136cc361d0bd01e52c1de9edfbf990a961a9bad
swmoon <swmoon@bluedigm.com>
2018-03-03 17:47:26 +0900

    Build.gradle 문제점 수정

--- src/main/java/org/openpaas/portal/registration/RegistrationServerApplication.java
+++ src/main/java/org/openpaas/portal/registration/RegistrationServerApplication.java
@@ -3,6 +3,7 @@ package org.openpaas.portal.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class RegistrationServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationServerApplication.class, args);
