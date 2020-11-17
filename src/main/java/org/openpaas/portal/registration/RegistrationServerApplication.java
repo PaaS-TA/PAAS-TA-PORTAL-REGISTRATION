33bd627566361bc14d05c1430e0cf1b60ebf094d
lena <lena@irevit.co.kr>
2020-11-17 11:10:41 +0900

    add manifest & update config

--- src/main/java/org/openpaas/portal/registration/RegistrationServerApplication.java
+++ src/main/java/org/openpaas/portal/registration/RegistrationServerApplication.java
@@ -3,7 +3,6 @@ package org.openpaas.portal.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -13,7 +12,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-@EnableDiscoveryClient
 public class RegistrationServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RegistrationServerApplication.class, args);
